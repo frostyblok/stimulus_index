a15fe054b2cdf4391c5a308528a2173245c758a6
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-03-27 10:33:46 +0100

    Setup Rspec for testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, :email, :title, :phone, :status, presence: true
+  validates_uniqueness_of :email
+
   enum status: {
     inactive: 0,
     active: 1
