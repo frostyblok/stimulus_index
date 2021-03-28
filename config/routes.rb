0c493be633dd4a2277dec8dc55bd4e6760ab3457
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-03-28 23:54:46 +0100

    Refactor routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: 'users#index'
+  root to: 'users#index'
 
-  resources :users
-  get '/users_json' => 'users#users_json'
+  resources :users do
+    member do
+      get '/users_json' => 'users#users_json'
+    end
+  end
 end
